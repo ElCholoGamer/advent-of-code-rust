ae4aec9c77fb9c0717cb47c86e0173029821211e
ElCholoGamer <josedanielgrayson@gmail.com>
2022-12-06 06:58:46 -0500

    2022 - day 6

---
+++ 2022/day6/src/main.rs
@@ -0,0 +1,43 @@
+use std::collections::HashMap;
+use aoc_lib::{AocSolution, BoxedError, Error};
+
+fn main() -> Result<(), BoxedError> {
+    aoc_lib::run::<Day6>(6)
+}
+
+fn find_marker(message: &str, marker_len: usize) -> Option<usize> {
+    let mut i = 0;
+    'main: while i < message.len() - marker_len {
+        let mut map = HashMap::new();
+
+        for (c_index, char) in message[i..(i + marker_len)].chars().enumerate() {
+            if let Some(previous_index) = map.insert(char, c_index) {
+                i += previous_index + 1;
+                continue 'main;
+            }
+        }
+
+        return Some(i + marker_len);
+    }
+
+    None
+}
+
+struct Day6;
+
+impl AocSolution for Day6 {
+    type Input = String;
+    type Output = usize;
+
+    fn parse_input(raw_input: String) -> Self::Input {
+        raw_input
+    }
+
+    fn part_1(input: &Self::Input) -> Result<Self::Output, BoxedError> {
+        find_marker(input, 4).ok_or(Error::Misc("could not find first marker".into()).into())
+    }
+
+    fn part_2(input: &Self::Input) -> Result<Self::Output, BoxedError> {
+        find_marker(input, 14).ok_or(Error::Misc("could not find first marker".into()).into())
+    }
+}
