3afc34b8b7cf06602f121aee98e96794db9d9d0c
ElCholoGamer <josedanielgrayson@gmail.com>
2022-12-06 00:13:35 -0500

    2022 - day 5

--- 2022/day5/src/main.rs
+++ 2022/day5/src/main.rs
@@ -1,4 +1,4 @@
-use aoc_lib::{AocSolution, BoxedError, Error};
+use aoc_lib::{AocSolution, BoxedError};
 
 fn main() -> Result<(), BoxedError> {
     aoc_lib::run::<Day5>(5)
@@ -16,8 +16,8 @@ impl From<&str> for Instruction {
         let words = s.split_whitespace().collect::<Vec<_>>();
         Self {
             amount: words[1].parse().unwrap(),
-            from: words[3].parse().unwrap(),
-            to: words[5].parse().unwrap(),
+            from: words[3].parse::<usize>().unwrap() - 1,
+            to: words[5].parse::<usize>().unwrap() - 1,
         }
     }
 }
@@ -25,7 +25,7 @@ impl From<&str> for Instruction {
 #[derive(Debug)]
 struct CrateInput {
     initial_setup: Vec<Vec<char>>,
-    procedure: Vec<Instruction>
+    procedure: Vec<Instruction>,
 }
 
 struct Day5;
@@ -38,15 +38,12 @@ impl AocSolution for Day5 {
         let lines = raw_input.lines().collect::<Vec<_>>();
         let separator = lines.iter().position(|l| l.is_empty()).unwrap();
 
-        let pile_count = lines[separator - 1].split_ascii_whitespace().count();
+        let pile_count = lines[separator - 1].split_whitespace().count();
         let mut initial_setup = vec![Vec::new(); pile_count];
 
         for &line in lines[..separator - 1].iter().rev() {
             for i in 0..pile_count {
-                let index = 1 + i * 4;
-                if line.len() <= index { break; }
-
-                let char = line.chars().nth(index).unwrap();
+                let char = line.chars().nth(1 + i * 4).expect("row too small");
                 if char != ' ' {
                     initial_setup[i].push(char);
                 }
@@ -63,8 +60,8 @@ impl AocSolution for Day5 {
         let mut piles = input.initial_setup.clone();
         for instruction in input.procedure.iter() {
             for _ in 0..instruction.amount {
-                let item = piles[instruction.from - 1].pop().expect("missing item on pile");
-                piles[instruction.to - 1].push(item);
+                let item = piles[instruction.from].pop().expect("missing item on pile");
+                piles[instruction.to].push(item);
             }
         }
 
@@ -75,16 +72,8 @@ impl AocSolution for Day5 {
         let mut piles = input.initial_setup.clone();
 
         for &Instruction { from, to, amount } in input.procedure.iter() {
-            let mut tmp = Vec::with_capacity(amount);
-            for _ in 0..amount {
-                let item = piles[from - 1].pop().expect("missing item on pile");
-                tmp.push(item);
-            }
-
-            for _ in 0..amount {
-                let item = tmp.pop().unwrap();
-                piles[to - 1].push(item);
-            }
+            let mut items = piles[from].drain((piles[from].len() - amount)..).collect();
+            piles[to].append(&mut items);
         }
 
         Ok(piles.iter().map(|pile| pile.last().unwrap_or(&' ')).collect::<String>())
