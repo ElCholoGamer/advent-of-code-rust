c39ad04ad0416b0721f484a666bd8d0319507273
ElCholoGamer <josedanielgrayson@gmail.com>
2022-12-06 16:43:58 -0500

    refactor 2022 day 6

--- 2022/day6/src/main.rs
+++ 2022/day6/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 use aoc_lib::{AocSolution, BoxedError, Error};
 
 fn main() -> Result<(), BoxedError> {
@@ -6,21 +6,10 @@ fn main() -> Result<(), BoxedError> {
 }
 
 fn find_marker(message: &str, marker_len: usize) -> Option<usize> {
-    let mut i = 0;
-    'main: while i < message.len() - marker_len {
-        let mut seen_char_indices = HashMap::new();
-
-        for (c_index, char) in message[i..(i + marker_len)].chars().enumerate() {
-            if let Some(previous_index) = seen_char_indices.insert(char, c_index) {
-                i += previous_index + 1;
-                continue 'main;
-            }
-        }
-
-        return Some(i + marker_len);
-    }
-
-    None
+    message.chars().collect::<Vec<_>>()
+        .windows(marker_len)
+        .position(|chars| chars.iter().collect::<HashSet<_>>().len() == chars.len())
+        .map(|sequence_index| sequence_index + marker_len)
 }
 
 struct Day6;
@@ -29,9 +18,7 @@ impl AocSolution for Day6 {
     type Input = String;
     type Output = usize;
 
-    fn parse_input(raw_input: String) -> Self::Input {
-        raw_input
-    }
+    fn parse_input(raw_input: String) -> Self::Input { raw_input }
 
     fn part_1(input: &Self::Input) -> Result<Self::Output, BoxedError> {
         find_marker(input, 4).ok_or(Error::Misc("could not find first marker".into()).into())
